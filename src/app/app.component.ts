cd68b499807bcdc6dc3fcd03a83c7cde90bad2c8
youpiyoful <youpiyoful@Host-002.lan>
2018-11-08 16:23:26 +0100

    :construction: create post-list component
    I create a post list component to handle my posts more easyly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-blog-angular';
+  post: {
+    title: string,
+    content: string,
+    loveIts: number,
+    created_at: Date
+  }
 }
