b2f09eb7f370564382bce46f7677a8d1f6f39b07
youpiyoful <youpiyoful@Host-002.lan>
2018-11-09 01:05:21 +0100

    :hankey: Finish the exo t1 openclassroom
    I have finish quickly the t1 angular exo by openclassrooms, finaly i have 3 components and using bootstrap quickly...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  post: {
-    title: string,
-    content: string,
-    loveIts: number,
-    created_at: Date
-  }
+  // lastUpdate = new Promise((resolve, reject) => {
+  //   const date = new Date();
+  //   setTimeout(
+  //     () => {
+  //       resolve(date);
+  //     }, 2000
+  //   );
+  // });
+
+  posts = [
+    {
+      title: 'test1',
+      content: 'quid multos tamen sunt illum duis multos dolor quorum malis',
+      loveIts: -1,
+      created_at: new Date()
+    },
+    {
+      title: 'test2',
+      content: 'noster cillum ipsum malis malis quid aute quae aliqua magna',
+      loveIts: 0,
+      created_at: Date
+    },
+    {
+      title: 'test3',
+      content: 'veniam nulla quis cillum culpa labore labore nulla malis malis',
+      loveIts: 2,
+      created_at: Date
+    }
+  ];
 }
--- src/app/post-list-item/post-list-item.component.ts
+++ src/app/post-list-item/post-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post-list-item',
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostListItemComponent implements OnInit {
 
-  constructor() { }
+  @Input() postTitle: string;
+  @Input() postContent: string;
+  @Input() postLikeElt: number;
+  @Input() postDate: Date;
+
+  constructor() {
+    this.postDate = new Date();
+  }
 
   ngOnInit() {
   }
 
+  upLike() {
+    if(isNaN(this.postLikeElt)){
+      this.postLikeElt = 0;
+    }
+    this.postLikeElt++;
+  }
+
+  downLike() {
+    if(isNaN(this.postLikeElt)){
+      this.postLikeElt = 0;
+    }
+    this.postLikeElt--;
+  }
+
+  getColor(){
+    if(this.postLikeElt > 0){
+      return 'green';
+    }
+    else if(this.postLikeElt < 0){
+      return 'red';
+    }
+    else{
+      return 'black';
+    }
+  }
 }
